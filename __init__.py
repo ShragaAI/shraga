442c48154fec08bb2e996ed5c126e7bdab54ee8a
lizozom <lizka.k@gmail.com>
2025-03-16 11:46:46 +0200

    init

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from shraga_common import (embedders, ingest, logging, models, preprocessing,
+                           prompts, retrievers, routing, services, utils)
+
+
+__all__ = ["embedders", "ingest", "logging", "models", "preprocessing", "prompts", "retrievers", "routing", "services", "utils"]
---
+++ shraga_common/retrievers/get_client.py
@@ -0,0 +1,16 @@
+from shraga_common import ShragaConfig
+
+from .common import RetrieverConfig
+from .elasticsearch import ElasticsearchRetriever
+from .opensearch import OpenSearchRetriever
+
+
+def get_client(shraga_config: ShragaConfig):
+    opensearch_config = shraga_config.get("retrievers.opensearch")
+    elasticsearch_config = shraga_config.get("retrievers.elasticsearch")
+    config: RetrieverConfig = opensearch_config or elasticsearch_config
+    RetrieverClass = (
+        OpenSearchRetriever if opensearch_config else ElasticsearchRetriever
+    )
+
+    return RetrieverClass.get_client(shraga_config, config)
