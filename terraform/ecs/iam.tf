13dc40c1184becd355a1647e6b07ad595503a995
Felipe Carvalho <felipe@bigdataboutique.com>
2025-04-07 17:08:45 -0300

    Quick fix

--- terraform/ecs/iam.tf
+++ terraform/ecs/iam.tf
@@ -17,14 +17,15 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  count      = local.should_create_ecs_task_execution_role ? 1 : 0
+  role       = aws_iam_role.ecs_task_execution_role[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_iam_role_policy" "ecs_task_execution_role_cw_logs" {
   count = local.should_create_cw_log_group ? 1 : 0
   name  = "cloudwatch-logs"
-  role  = aws_iam_role.ecs_task_execution_role.name
+  role  = aws_iam_role.ecs_task_execution_role[0].name
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
