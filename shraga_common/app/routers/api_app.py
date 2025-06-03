6d69a0220dda793a568e417e532dc982e7d751dd
Alex Doro <alexdoro.work@gmail.com>
2025-06-03 15:49:11 +0200

    fix: code refinements

--- shraga_common/app/routers/api_app.py
+++ shraga_common/app/routers/api_app.py
@@ -77,7 +77,7 @@ def load_api_app():
             ret = {
                 "display_name": display_name,
                 "shraga_version": get_git_commit() or "unknown",
-                "lifetime": shraga_config.get("auth.lifetime")
+                "session_lifetime_hrs": shraga_config.get("auth.session_lifetime_hrs", 24)
                 # "identity": request.user.identity,
             }
             if is_analytics_authorized(display_name):
